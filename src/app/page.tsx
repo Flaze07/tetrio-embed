7d9275fdb034206f7871717c330bcbff08ca5d81
Flaze07 <Flaze07@users.noreply.github.com>
2024-09-17 13:24:24 +0700

    added obtaining user image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,33 @@ type Props = {
 }
 
 export async function generateMetadata( { searchParams }: Props, parent: ResolvingMetadata ): Promise<Metadata> {
-    const id = searchParams.id;
+    const name = searchParams.name;
+    
+    if(name === undefined) {
+        return {
+            title: "Goblok",
+            description: "Goblok, pake ?name=apapun"
+        }
+    }
+
+    const res = await fetch(`https://ch.tetr.io/api/users/${name}`);
+    const data: any = await res.json();
+
+    if(data.success === false) {
+        return {
+            title: "Goblok",
+            description: "Gak ada user dengan nama ini tolol"
+        }
+    }
 
     return {
-        title: "title",
-        description: `Page ${id}`,
+        title: `${name}`,
+        description: `Data user ${name}`,
+        openGraph: {
+            images: [
+                `https://tetr.io/user-content/avatars/${data.data._id}.jpg?rv=${data.data.avatar_revision}`
+            ]
+        }
     }
 };
 
