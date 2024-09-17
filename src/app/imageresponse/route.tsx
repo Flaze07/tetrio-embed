a5b04b76792d30250da2354d5589dad9d559faf1
Flaze07 <Flaze07@users.noreply.github.com>
2024-09-17 15:44:50 +0700

    many modif

--- src/app/imageresponse/route.tsx
+++ src/app/imageresponse/route.tsx
@@ -2,26 +2,38 @@ import type { NextRequest } from 'next/server';
 import { ImageResponse } from 'next/og';
 
 export async function GET(request: NextRequest) {
-    return  new ImageResponse(
-       (
-         <div
-           style={{
-             fontSize: 128,
-             background: 'white',
-             width: '100%',
-             height: '100%',
-             display: 'flex',
-             textAlign: 'center',
-             alignItems: 'center',
-             justifyContent: 'center',
-           }}
-         >
-           Hello world!
-         </div>
-       ),
-       {
-         width: 1200,
-         height: 600,
-       },
-     );
- }
+    const name = request.nextUrl.searchParams.get('name') || 'World';
+    
+    return new ImageResponse(
+        (
+            <div>
+                <h1>Hello {name}</h1>
+            </div>
+        )
+    )
+}
+
+// export async function GET(request) {
+//     return  new ImageResponse(
+//        (
+//          <div
+//            style={{
+//              fontSize: 128,
+//              background: 'white',
+//              width: '100%',
+//              height: '100%',
+//              display: 'flex',
+//              textAlign: 'center',
+//              alignItems: 'center',
+//              justifyContent: 'center',
+//            }}
+//          >
+//            Hello world!
+//          </div>
+//        ),
+//        {
+//          width: 1200,
+//          height: 600,
+//        },
+//      );
+//  }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,96 +1,105 @@
-// import type { Metadata, ResolvingMetadata } from "next";
+
+// // import type { Metadata, ResolvingMetadata } from "next";
+// // import { ImageResponse } from "next/og";
+
+// // export const runtime = 'edge'
+
+// // type Props = {
+// //     searchParams: { [key: string]: string | string[] | undefined }
+// // }
+
+// // export async function generateMetadata( { searchParams }: Props, parent: ResolvingMetadata ): Promise<Metadata> {
+// //     const name = searchParams.name;
+    
+// //     if(name === undefined) {
+// //         return {
+// //             title: "Goblok",
+// //             description: "Goblok, pake ?name=apapun"
+// //         }
+// //     }
+
+// //     const res = await fetch(`https://ch.tetr.io/api/users/${name}`);
+// //     const data: any = await res.json();
+
+// //     if(data.success === false) {
+// //         return {
+// //             title: "Goblok",
+// //             description: "Gak ada user dengan nama ini tolol"
+// //         }
+// //     }
+
+// //     const generatedImage = new ImageResponse(
+// //         (
+// //             // ImageResponse JSX element
+// //             <div
+// //             style={{
+// //                 fontSize: 128,
+// //                 background: 'white',
+// //                 width: '100%',
+// //                 height: '100%',
+// //                 display: 'flex',
+// //                 alignItems: 'center',
+// //                 justifyContent: 'center',
+// //             }}
+// //             >
+// //             This is a test
+// //             </div>
+// //         )
+// //     )
+
+// //     return {
+// //         title: `${name}`,
+// //         description: `Data user ${name}`,
+// //         openGraph: {
+// //             images: [
+// //                 generatedImage
+// //             ]
+// //         }
+// //     }
+// // };
+
+// // export default function Page() {
+// //     return (
+// //         <div>
+// //             <h1>Hello World</h1>
+// //         </div>
+// //     )
+// // }
+
 // import { ImageResponse } from "next/og";
 
-// export const runtime = 'edge'
+// export const runtime = 'edge';
 
 // type Props = {
 //     searchParams: { [key: string]: string | string[] | undefined }
 // }
 
-// export async function generateMetadata( { searchParams }: Props, parent: ResolvingMetadata ): Promise<Metadata> {
-//     const name = searchParams.name;
-    
-//     if(name === undefined) {
-//         return {
-//             title: "Goblok",
-//             description: "Goblok, pake ?name=apapun"
-//         }
-//     }
-
-//     const res = await fetch(`https://ch.tetr.io/api/users/${name}`);
-//     const data: any = await res.json();
-
-//     if(data.success === false) {
-//         return {
-//             title: "Goblok",
-//             description: "Gak ada user dengan nama ini tolol"
-//         }
-//     }
-
-//     const generatedImage = new ImageResponse(
+// export default async function Page(
+//     { searchParams }: Props,
+// ): Promise<any> {
+//     return new ImageResponse(
 //         (
-//             // ImageResponse JSX element
 //             <div
-//             style={{
-//                 fontSize: 128,
-//                 background: 'white',
-//                 width: '100%',
-//                 height: '100%',
-//                 display: 'flex',
-//                 alignItems: 'center',
-//                 justifyContent: 'center',
-//             }}
+//                 style={{
+//                     fontSize: 128,
+//                     background: 'white',
+//                     width: '100%',
+//                     height: '100%',
+//                     display: 'flex',
+//                     alignItems: 'center',
+//                     justifyContent: 'center',
+//                 }}
 //             >
-//             This is a test
+//                 This is a test
 //             </div>
 //         )
 //     )
-
-//     return {
-//         title: `${name}`,
-//         description: `Data user ${name}`,
-//         openGraph: {
-//             images: [
-//                 generatedImage
-//             ]
-//         }
-//     }
-// };
-
-// export default function Page() {
-//     return (
-//         <div>
-//             <h1>Hello World</h1>
-//         </div>
-//     )
 // }
 
-import { ImageResponse } from "next/og";
-
-export const runtime = 'edge';
-
-type Props = {
-    searchParams: { [key: string]: string | string[] | undefined }
-}
-
-export default async function Page(
-    { searchParams }: Props,
-): Promise<any> {
-    return new ImageResponse(
-        (
-            <div
-                style={{
-                    fontSize: 128,
-                    background: 'white',
-                    width: '100%',
-                    height: '100%',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}
-            >
-                This is a test
-            </div>
-        )
+export default async function Page() {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
     )
 }
