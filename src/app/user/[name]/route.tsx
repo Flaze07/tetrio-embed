92da489f6140897db6a9d9a656d22a3dcf948b29
Flaze07 <Flaze07@users.noreply.github.com>
2024-09-17 18:56:25 +0700

    test progress

--- src/app/user/[name]/route.tsx
+++ src/app/user/[name]/route.tsx
@@ -1,25 +1,143 @@
 import type { NextRequest } from "next/server";
 import { ImageResponse } from "next/og";
+import bg from '@/app/resource/background.png';
 
 export const runtime = 'edge';
 
 export async function GET(req: NextRequest, { params }: { params: { name: string } }) {
     const { name } = params;
 
+    const resp = await fetch(`https://ch.tetr.io/api/users/${name}`);
+    const data: any = await resp.json();
+
+    if(data.success === false) {
+        return new ImageResponse(
+            (
+                <div
+                    style={{
+                        fontSize: 64,
+                        background: 'white',
+                        width: '100%',
+                        height: '100%',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        paddingLeft: '100px',
+                        // backgroundImage: `url(https://ik.imagekit.io/flaze002111/background.png)`,
+                        // backgroundRepeat: 'repeat',
+                        backgroundColor: '#363636',
+                        color: 'lightgray',
+                        // textShadow: '-2px -2px 0 #fff, 2px -2px 0 #fff, -2px 2px 0 #fff, 2px 2px 0 #fff'
+                    }}
+                >
+                    <span
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <span
+                            style={{
+                                fontSize: 128,
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            Goblok
+                        </span>
+                        <span>
+                            Gak ada user dengan nama ini tolol
+                        </span>
+                    </span>
+                    <span
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignContent: 'flex-end',
+                            marginLeft: '100px'
+                        }}
+                    >
+                        <img 
+                            src="https://ik.imagekit.io/flaze002111/crazy.png"
+                            style={{
+                                width: '500px',
+                            }}
+                        />
+                    </span>
+                </div>
+            ),
+            {
+                width: 1800,
+                height: 600
+            }
+        )
+    }
+
+    const avatarSource: string = `https://tetr.io/user-content/avatars/${data.data._id}.jpg?rv=${data.data.avatar_revision}`;
+
+    const userSummaryResp = await fetch(`https://ch.tetr.io/api/users/${name}/summaries`);
+    const userSummaryData: any = await userSummaryResp.json();
+    // console.log(userSummaryData.data.league.rank);
+
     return new ImageResponse(
         (
             <div
                 style={{
-                    fontSize: 64,
+                    // fontFamily: ''
+                    fontSize: 48,
                     background: 'white',
                     width: '100%',
                     height: '100%',
                     display: 'flex',
+                    flexDirection: 'row',
                     alignItems: 'center',
-                    justifyContent: 'center'
+                    paddingLeft: '100px',
+                    // backgroundImage: `url(https://ik.imagekit.io/flaze002111/background.png)`,
+                    // backgroundRepeat: 'repeat',
+                    backgroundColor: '#363636',
+                    color: 'lightgray',
+                    // textShadow: '-2px -2px 0 #fff, 2px -2px 0 #fff, -2px 2px 0 #fff, 2px 2px 0 #fff'
                 }}
             >
-                <h1>Hello {name}</h1>
+                <img 
+                    src={avatarSource} 
+                    style={{
+                        width: '300px'
+                    }}
+                />
+                <span
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '70%',
+                        alignItems: 'flex-start',
+                        marginLeft: '50px'
+                    }}
+                >
+                    <span
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'flex-start',
+                        }}
+                    >
+                        <p
+                            style={{
+                                fontSize: 76,
+                                fontWeight: 'bold',
+                            }}
+                        >
+                            {name}
+                        </p>
+                        <img 
+                            src={`https://tetr.io/res/league-ranks/${userSummaryData.data.league.rank}.png`} 
+                            style={{
+                                width: '100px',
+                                marginTop: '50px',
+                                marginLeft: '20px'
+                            }}
+                        />
+                    </span>
+                </span>
             </div>
         ),
         {
