3fc3fbe45a61e768fbf3544d0fe41ee24779958e
Flaze07 <Flaze07@users.noreply.github.com>
2024-09-19 19:16:38 +0700

    modified looks

--- src/app/user/[name]/route.tsx
+++ src/app/user/[name]/route.tsx
@@ -1,9 +1,10 @@
 import type { NextRequest } from "next/server";
-import Image from 'next/image';
+import { Inter } from 'next/font/google';
 import { ImageResponse } from "next/og";
 import bg from '@/app/resource/background.png';
 
 export const runtime = 'edge';
+const inter = Inter({ subsets: ['latin'] })
 
 // const errorImage = new ImageResponse(
 //     (
@@ -37,8 +38,12 @@ function convertMilliseconds(ms: number): string {
 }
 
 export async function GET(req: NextRequest, { params }: { params: { name: string } }) {
+    const imageSize = {
+        width: 885,
+        height: 698
+    }
     const { name } = params;
-
+    
     const resp = await fetch(`https://ch.tetr.io/api/users/${name}`);
     const data: any = await resp.json();
 
@@ -116,106 +121,273 @@ export async function GET(req: NextRequest, { params }: { params: { name: string
 
     const userSummaryResp = await fetch(`https://ch.tetr.io/api/users/${name}/summaries`);
     const userSummaryData: any = await userSummaryResp.json();
-    // console.log(userSummaryData.data.league.rank);
 
+    //40L
     const result40L: string = userSummaryData.data['40l'].record === null ? 'N/A' : convertMilliseconds(userSummaryData.data['40l'].record.results.stats.finaltime);
+    const rankLocal40L: string = userSummaryData.data['40l'] === null ? 'N/A': userSummaryData.data['40l'].rank_local;
+    const rank40L: string = userSummaryData.data['40l'] === null ? 'N/A': userSummaryData.data['40l'].rank;
+
+    //blitz
     const resultBlitz: string = userSummaryData.data.blitz.record === null ? 'N/A' : userSummaryData.data.blitz.record.results.stats.score;
+    const rankLocalBlitz: string = userSummaryData.data.blitz === null ? 'N/A': userSummaryData.data.blitz.rank_local;
+    const rankBlitz: string = userSummaryData.data.blitz === null ? 'N/A': userSummaryData.data.blitz.rank
+
+    //League
+    const resultLeague: string = userSummaryData.data.league.record === null ? 'N/A' : userSummaryData.data.league.tr;
+    const rankLocalLeague: string = userSummaryData.data.league === null ? 'N/A': userSummaryData.data.league.standing_local;
+    const rankLeague: string = userSummaryData.data.league === null ? 'N/A': userSummaryData.data.league.standing;
+    const rankIconLeague: string = userSummaryData.data.league === null ? 'N/A': userSummaryData.data.league.rank;
 
     return new ImageResponse(
         (
             <div
+                // className={inter.className}
                 style={{
-                    // fontFamily: ''
+                    // fontFamily: 'Helvetica',
                     fontSize: 68,
                     background: 'white',
                     width: '100%',
                     height: '100%',
                     display: 'flex',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    paddingLeft: '100px',
-                    backgroundColor: '#363636',
+                    flexDirection: 'column',
+                    backgroundColor: '#E5D9F2',
                     color: 'lightgray',
+                    border: "9px solid #060606",
+                    boxShadow: "inset 0px 0px 8.5px 21px rgba(0, 0, 0, 0.25)",
+                    paddingTop: "47px",
+                    paddingLeft: '37px',
+                    // paddingRight: '300px',
                 }}
             >
-                <img 
-                    src={avatarSource} 
-                    style={{
-                        width: '300px'
-                    }}
-                />
                 <span
                     style={{
                         display: 'flex',
-                        flexDirection: 'column',
-                        height: '70%',
-                        marginLeft: '50px',
-                        marginTop: '70px',
-                        // justifyContent: 'center'
+                        flexDirection: 'row',
+                        gap: '25px',
+                        marginRight: '300px'
                     }}
                 >
+                    <img 
+                        src={`${avatarSource}`} 
+                        style={{
+                            width: '215px',
+                            borderRadius: '90px',
+                            boxShadow: '5px 6px 6px 3px rgba(0, 0, 0, 0.25)'
+                        }}
+                    />
                     <span
                         style={{
                             display: 'flex',
-                            flexDirection: 'row',
-                            // backgroundColor: 'blue',
+                            flexDirection: 'column',
+                            width: '100%',
+                            fontSize: 40,
+                            color: '#000'
+                        }}
+                    >
+                        <span
+                        >
+                            {name}
+                        </span>
+                        <span
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                flex: 1,
+                                width: '100%',
+                                boxShadow: '5px 6px 4px rgba(0, 0, 0, 0.25)',
+                                border: '4px solid #000000',
+                                borderRadius: '20px',
+                                backgroundColor: '#CDC1FF',
+                                paddingLeft: '28px',
+                            }}
+                            >
+                            <span
+                                style={{
+                                    paddingTop: '23px',
+                                    display: 'flex',
+                                    flexDirection: 'column'
+                                }}
+                            >
+                                <span
+                                    style={{
+                                        display: 'flex',
+                                        flexDirection: 'row',
+                                        fontSize: 20,
+                                        gap: '13px'
+                                    }}
+                                >
+                                    <span
+                                        style={{
+                                            backgroundColor: ' #A594F9',
+                                            border: '3px solid #000000',
+                                            borderRadius: '20px',
+                                            padding: '8px 4px'
+                                        }}
+                                    >
+                                        Country#{rankLocalLeague}
+                                    </span>
+                                    <span
+                                        style={{
+                                            backgroundColor: ' #A594F9',
+                                            border: '3px solid #000000',
+                                            borderRadius: '20px',
+                                            padding: '8px 4px'
+                                        }}
+                                    >
+                                        Global#{rankLeague}
+                                    </span>
+                                </span>
+                                <span
+                                    style={{
+                                        fontSize: 48
+                                    }}
+                                >
+                                    {Math.floor(Number.parseFloat(resultLeague))} TR
+                                </span>
+                            </span>
+                            <span
+                                style={{
+                                    flex: 1
+                                }}
+                            >
+
+                            </span>
+                            <span>
+                                <img
+                                    src={`https://tetr.io/res/league-ranks/${rankIconLeague}.png`}
+                                    style={{
+                                        // alignSelf: 'flex-end',
+                                        width: '159px',
+                                        // height: '200px',
+                                        borderRadius: '20px',
+                                        // boxShadow: '5px 6px 6px 3px rgba(0, 0, 0, 0.25)'
+                                        
+                                    }}
+                                />
+                            </span>
+                        </span>
+                    </span>
+                </span>
+                <span
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        height: '141px',
+                        marginTop: '22px',
+                        marginRight: '55px',
+                        gap: '14px',
+                        color: '#000',
+                    }}
+                >
+                    {/* For 40L */}
+                    <span
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            flex: 1,
+                            backgroundColor: '#CDC1FF',
+                            border: '4px solid #000000',
+                            boxShadow: '5px 6px 4px rgba(0, 0, 0, 0.25)',
+                            borderRadius: '20px',
+                            paddingTop: '12px',
+                            paddingLeft: '20px'
                         }}
                     >
                         <span
                             style={{
-                                fontSize: 100,
-                                fontWeight: 'bold',
+                                display: 'flex',
+                                flexDirection: 'row',
+                                fontSize: 20,
+                                gap: '13px',
                             }}
                         >
-                            {name}
+                            <span
+                                style={{
+                                    backgroundColor: ' #A594F9',
+                                    border: '3px solid #000000',
+                                    borderRadius: '20px',
+                                    padding: '8px 4px'
+                                }}
+                            >
+                                Country#{rankLocal40L}
+                            </span>
+                            <span
+                                style={{
+                                    backgroundColor: ' #A594F9',
+                                    border: '3px solid #000000',
+                                    borderRadius: '20px',
+                                    padding: '8px 4px'
+                                }}
+                            >
+                                Country#{rank40L}
+                            </span>
                         </span>
-                        <img 
-                            src={`https://tetr.io/res/league-ranks/${userSummaryData.data.league.rank}.png`} 
-                            style={{
-                                width: '150px',
-                                display: 'block',
-                                marginLeft: '40px',
-                                // backgroundColor: 'red'
-                            }}
-                        />
+                        <span>
+                            <span
+                                style={{
+                                    fontSize: 48
+                                }}
+                            >
+                                {result40L}
+                            </span>
+                        </span>
                     </span>
+                    {/* For Blitz */}
                     <span
                         style={{
                             display: 'flex',
-                            flexDirection: 'row',
-                            marginTop: '20px',
+                            flexDirection: 'column',
+                            flex: 1,
+                            backgroundColor: '#CDC1FF',
+                            border: '4px solid #000000',
+                            boxShadow: '5px 6px 4px rgba(0, 0, 0, 0.25)',
+                            borderRadius: '20px',
+                            paddingTop: '12px',
+                            paddingLeft: '20px'
                         }}
-                        >
+                    >
                         <span
                             style={{
-                                color: 'black',
-                                backgroundColor: '#64ABED',
-                                boxShadow: 'inset 0px 0px 3.4px 5px rgba(0, 0, 0, 0.25)',
-                                padding: '5px 20px',
-                                borderRadius: '20px'
+                                display: 'flex',
+                                flexDirection: 'row',
+                                fontSize: 20,
+                                gap: '13px',
                             }}
                         >
-                            40L: {result40L}
+                            <span
+                                style={{
+                                    backgroundColor: ' #A594F9',
+                                    border: '3px solid #000000',
+                                    borderRadius: '20px',
+                                    padding: '8px 4px'
+                                }}
+                            >
+                                Country#{rankLocalBlitz}
+                            </span>
+                            <span
+                                style={{
+                                    backgroundColor: ' #A594F9',
+                                    border: '3px solid #000000',
+                                    borderRadius: '20px',
+                                    padding: '8px 4px'
+                                }}
+                            >
+                                Country#{rankBlitz}
+                            </span>
                         </span>
-                        <span
-                            style={{
-                                color: 'black',
-                                backgroundColor: '#DDEE73',
-                                boxShadow: 'inset 0px 0px 3.4px 5px rgba(0, 0, 0, 0.25)',
-                                padding: '5px 20px',
-                                borderRadius: '20px',
-                                marginLeft: '40px'
-                            }}
-                        >
-                            Blitz: {resultBlitz}
+                        <span>
+                            <span
+                                style={{
+                                    fontSize: 48
+                                }}
+                            >
+                                {resultBlitz}
+                            </span>
                         </span>
                     </span>
                 </span>
             </div>
         ),
-        {
-            width: 1800,
-            height: 600
-        }
+        imageSize,
     );
 }
